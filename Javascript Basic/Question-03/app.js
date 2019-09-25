814f27669703b490cf31cb4676a2c692361ec9dd
Hudson Holanda <hudsonholanda55@gmail.com>
2019-09-24 22:03:00 -0300

    Adicionando questão 03.

---
+++ Javascript Basic/Question-03/app.js
@@ -0,0 +1,10 @@
+let date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+let prefix = month < 10 ? "0" : "";
+
+let output = day + "/" + prefix + (month + 1 ) + "/" + year;
+
+console.log(output);
