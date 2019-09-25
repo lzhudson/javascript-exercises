1cd7b9b90ce801456b35360ab6842743d31e137a
Hudson Holanda <hudsonholanda55@gmail.com>
2019-09-24 22:31:36 -0300

    Adicionando questão 05.

---
+++ Javascript Basic/Question-05/app.js
@@ -0,0 +1,10 @@
+function animateString(id) {
+  let element = document.getElementById(id);
+  let textNode = element.childNodes[0];
+  let text = textNode.data;
+  console.log(text);
+  setInterval(function (){
+    text = text[text.length -1] + text.substring(0, text.length - 1);
+    textNode.data = text;
+    }, 100)
+}
