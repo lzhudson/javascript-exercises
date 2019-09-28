70d34d6130b2656e55e3e67e6e996998b2e7ff84
Hudson Holanda <hudsonholanda55@gmail.com>
2019-09-28 11:25:09 -0300

    Adicionando questão 23.

---
+++ Javascript Basic/Question-23/app.js
@@ -0,0 +1,11 @@
+function newSequence(string) {
+  if(string.length > 1) {
+    let newfirstChar = string[string.length - 1];
+    let newlastChar = string[0]; 
+    let newSequence = newfirstChar + string.slice(1, string.length - 1) + newlastChar;
+    return newSequence;
+  } else {
+    return 'Not value equal == 1';
+  }
+}
+console.log(newSequence('Hudson'));
