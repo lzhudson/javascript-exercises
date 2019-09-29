4a580cd19d9dd213cbdf225747e6a83a76de9911
Hudson Holanda <hudsonholanda55@gmail.com>
2019-09-29 10:46:12 -0300

    Adicionando questão 29.

---
+++ Javascript Basic/Question-29/app.js
@@ -0,0 +1,11 @@
+function inBetween50a99(value1, value2) {
+  if((value1 > 50 && value2 < 99) || (value2 > 50 && value2 < 99)) {
+      return true;
+  } else {
+    return false;
+  }
+}
+console.log(inBetween50a99(20, 60));
+console.log(inBetween50a99(75, 10));
+console.log(inBetween50a99(65, 72));
+console.log(inBetween50a99(20, 30));
