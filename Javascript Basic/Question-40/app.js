d9a092bac9f0964398b458b821b5f7cb85e85ab6
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-01 11:58:14 -0300

    Adicionando questão 40.

---
+++ Javascript Basic/Question-40/app.js
@@ -0,0 +1,13 @@
+function verifySum8OrDiference8(num1, num2) {
+  const sumNumbers = num1 + num2;
+  const difNumbers = num1 - num2;
+  if(num1 === 8 || num2 === 8 || sumNumbers === 8 || difNumbers === 8) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(verifySum8OrDiference8(8,8));
+console.log(verifySum8OrDiference8(4,4));
+console.log(verifySum8OrDiference8(8,2));
+console.log(verifySum8OrDiference8(2,2));
