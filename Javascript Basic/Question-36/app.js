91211131d5b55585107eda359771f7a622577445
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-01 11:57:04 -0300

    Adicionando questão 36.

--- Javascript Basic/Question-36/app.js
+++ Javascript Basic/Question-36/app.js
@@ -8,11 +8,12 @@ function isIntergerThreeNumbers(num1,num2,num3) {
   let lastNumberNumOne = numOne[numOne.length - 1];
   let lastNumberNumTwo = numTwo[numTwo.length - 1];
   let lastNumberNumThree = numThree[numThree.length - 1];
-  console.log(lastNumberNumOne, lastNumberNumTwo, lastNumberNumThree);
   if(lastNumberNumOne === lastNumberNumTwo && lastNumberNumOne === lastNumberNumThree) {
     return true;
   } else {
-    false;
+    return false;
   }
 }
 console.log(isIntergerThreeNumbers(123,456,789));
+console.log(isIntergerThreeNumbers(123,453,786));
+console.log(isIntergerThreeNumbers(123,453,783));
