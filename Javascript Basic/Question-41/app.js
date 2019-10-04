7bdbc9041b6f66cf30f021de4dcd4cbf33e1da81
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-04 00:53:19 -0300

    Adicionando questão 41.

---
+++ Javascript Basic/Question-41/app.js
@@ -0,0 +1,12 @@
+function checkEqualNumbers(num1, num2,num3) {
+  if(num1 === num2 && num1 === num3) {
+    return 30;
+  } else if(num1 !== num2 && num1 !== num3 && num2 !== num3) {
+    return 20;
+  } else if(num1 === num2 || num2 === num3 || num3 === num1) {
+    return 40;
+  }
+}
+console.log(checkEqualNumbers(10,20,30));
+console.log(checkEqualNumbers(10,10, 20));
+console.log(checkEqualNumbers(30,30,30));
