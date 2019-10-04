c31fc840632e9669c81fe58851ea1a5e1ae54636
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-04 00:52:26 -0300

    Refatorando codigo questão 05.

--- Javascript Basic/Question-05/app.js
+++ Javascript Basic/Question-05/app.js
@@ -2,7 +2,6 @@ function animateString(id) {
   let element = document.getElementById(id);
   let textNode = element.childNodes[0];
   let text = textNode.data;
-  console.log(text);
   setInterval(function (){
     text = text[text.length -1] + text.substring(0, text.length - 1);
     textNode.data = text;
