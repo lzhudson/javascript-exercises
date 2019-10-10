320ffff094a1d1c4735ee4ca45a1427cd164415a
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-10 14:59:43 -0300

    Refatorando codigo.

--- Javascript Basic/Question-07/app.js
+++ Javascript Basic/Question-07/app.js
@@ -1,12 +1,8 @@
-let date = new Date();
-
-let month = date.setMonth(0);
-let dayDate = date.setDate(1);
 
 let daysList = ['Domingo','Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado'];
 
 for(let i = 2014; i < 2050; i++) {
-  let year = date.setFullYear(i);
+  let date = new Date(i, 0, 1);
   if(daysList[date.getDay()] === 'Domingo') {
     console.log(date.getFullYear());
   }
