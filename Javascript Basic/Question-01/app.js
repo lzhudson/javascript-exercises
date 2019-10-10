9c9de81721cd79607baa1fd2b69b4e5c0895f412
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-10 14:53:47 -0300

    Refatorando codigo, colocando operador ternario na definição de sufixo e adicionando regra para quando a hora for a partir da meia noite ou meio dia.

--- Javascript Basic/Question-01/app.js
+++ Javascript Basic/Question-01/app.js
@@ -2,12 +2,25 @@ let date = new Date();
 let daysArray = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 let day = daysArray[date.getDay()];
 let hours = date.getHours();
-let sufix = "";
-if(hours > 12) {
-  sufix = "PM";
-  hours -= 12;
-} else {
-  sufix = "AM";
+let sufix = (hours > 12) ? "PM" : "AM";
+hours = (hours > 12) ? hours -= 12 : hours;
+if(hours === 0 && sufix == "PM") {
+  if(minutes === 0 && seconds === 0) {
+    hours = 12;
+    sufix = ' Noon';
+  } else {
+    hours = 12;
+    sufix = "PM";
+  }
+}
+if(hours === 0 && sufix === "PM") {
+  if(minutes === 0 && seconds === 0) {
+    hours = 12;
+    sufix = " Midnight";
+  } else {
+    hours = 12;
+    sufix = " AM"
+  }
 }
 let minutes = date.getMinutes();
 let seconds = date.getSeconds();
