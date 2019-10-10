2717b87641b8e967c9ed66c0b83c190ec561dfe4
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-10 14:55:32 -0300

    Corrindo função do prefixo.

--- Javascript Basic/Question-03/app.js
+++ Javascript Basic/Question-03/app.js
@@ -3,7 +3,7 @@ let date = new Date();
 let day = date.getDate();
 let month = date.getMonth();
 let year = date.getFullYear();
-let prefix = month < 10 ? "0" : "";
+let prefix = month < 10 ? "" : "0";
 
 let output = day + "/" + prefix + (month + 1 ) + "/" + year;
 
