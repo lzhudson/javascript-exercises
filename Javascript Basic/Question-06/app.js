0dd111b2834230a04f23a99258626b6976defc51
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-10 14:58:22 -0300

    Adicionando uma segunda variavel para testar a função.

--- Javascript Basic/Question-06/app.js
+++ Javascript Basic/Question-06/app.js
@@ -1,6 +1,7 @@
 let year = 2020;
+let yearTwo = 2021;
 function isLeap(year) {
   return year % 4 === 0 ? (year % 100 !== 0) : (year % 400 === 0);
 }
 console.log(isLeap(year) === true ? year + ' is leap' : year + ' is not leap.');
-console.log(isLeap(2021) === true ? year + ' is leap' : year + ' is not leap.')
+console.log(isLeap(yearTwo) === true ? yearTwo + ' is leap' : yearTwo + ' is not leap.')
