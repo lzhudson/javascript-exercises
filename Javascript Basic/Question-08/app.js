5f7b23b89943f57f242f929a3aed9921ada82791
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-10 15:01:00 -0300

    Refatorando codigo.

--- Javascript Basic/Question-08/app.js
+++ Javascript Basic/Question-08/app.js
@@ -1,7 +1,3 @@
 let randomNumber = Math.ceil(Math.random() * 10) + 1; 
 let number = window.prompt('Digite um numero');
-if(Number(number) === randomNumber) {
-  alert('Good Work');
-} else {
-  alert('Not Matched');
-}
+Number(number) === randomNumber ? alert('Good Work') :alert('Not Matched');
