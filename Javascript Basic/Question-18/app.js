b4b1bdaec3e678215d9ddf6eb930ba6f43fb4547
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-11 12:19:43 -0300

    Transformando o codigo em uma função e testando a maioria das situações que podem ser previstas.

--- Javascript Basic/Question-18/app.js
+++ Javascript Basic/Question-18/app.js
@@ -1,8 +1,11 @@
-let num1 = 20;
-let num2 = 30;
-let sum = num1 + num2;
-if(sum === 50 || num1 === 50 || num2 === 50) {
-  console.log(true);
-} else {
-  console.log(false);
+function checkTwoNumbers(num1, num2) {
+  if((num1 + num2) === 50 || num1 === 50 || num2 === 50) {
+    return true;
+  } else {
+    return false;
+  }
 }
+console.log(checkTwoNumbers(20,30));
+console.log(checkTwoNumbers(50, 10));
+console.log(checkTwoNumbers(2, 50));
+console.log(checkTwoNumbers(10,20));
