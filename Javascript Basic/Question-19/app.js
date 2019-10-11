7cfa7c61dee7693a9fe2f2beb32bf272744a78bf
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-11 12:21:16 -0300

    Transformando o codigo em função.

--- Javascript Basic/Question-19/app.js
+++ Javascript Basic/Question-19/app.js
@@ -1,6 +1,10 @@
-let num = 360;
-if(num >=20 && num <= 100) {
-  console.log(true);
-} else if(num >= 20 && num <= 400) {
-  console.log(true);
+function checkIntervalNumber(num) {
+  if(num >=20 && num <= 100) {
+    return true;
+  } else if(num >= 20 && num <= 400) {
+    return false;
+  }
 }
+console.log(checkIntervalNumber(10));
+console.log(checkIntervalNumber(40));
+console.log(checkIntervalNumber(350));
