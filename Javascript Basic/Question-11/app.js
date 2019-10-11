87e38690dbea1564eeca2a2e7cda52137544fc90
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-11 12:09:45 -0300

    Removendo variavel que não precisa ser declarada. Refatorando codigo.

--- Javascript Basic/Question-11/app.js
+++ Javascript Basic/Question-11/app.js
@@ -1,11 +1,9 @@
 function celsiusToFahrenheit(temp) {
-  let celsius = temp;
-  let fahrenheit = (celsius * 9 / 5) + 32;
+  let fahrenheit = (temp * 9 / 5) + 32;
   return fahrenheit + 'ºF';
 }
 function fahrenheitToCelsius(temp) {
-  let fahrenheit = temp; 
-  let celsius = (fahrenheit - 32) / (9 / 5);
+  let celsius = (temp - 32) / (9 / 5);
   return celsius + 'ºC';
 }
 console.log(celsiusToFahrenheit(60));
