aadd00b513d2f17e601a440ab107de1e4b875a67
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-11 12:14:20 -0300

    Criando uma função para acoplar o codigo e removendo as variaveis.

--- Javascript Basic/Question-16/app.js
+++ Javascript Basic/Question-16/app.js
@@ -1,10 +1,5 @@
-let num1 = 20;
-let num2 = 20;
-let sum;
-if(num1 === num2) {
-  sum = (num1 + num2) * 3;
-  console.log(sum);
-} else {
-  sum = num1 + num2;
-  console.log(sum);
+function calculateSum(num1, num2) {
+  var sum = num1 === num2 ? (num1 + num2) * 3 : num1 + num2;
+  return sum;
 }
+console.log(calculateSum(20, 30));
