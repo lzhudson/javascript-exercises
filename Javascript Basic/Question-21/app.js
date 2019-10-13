811450f32e931b20ff93f5d086663a901971964b
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-12 22:37:45 -0300

    Adicionando operador ternario e simplificando a condição.

--- Javascript Basic/Question-21/app.js
+++ Javascript Basic/Question-21/app.js
@@ -1,9 +1,5 @@
 function addPyString(value) {
-  if(value.slice(0,2) == 'Py'){
-    return value;
-  } else {
-  return 'Py' + value;
-  }
+  return value.slice(0,2 == 'Py') ? value : 'Py' + value;
 }
 console.log(addPyString('Py'));
 console.log(addPyString('oi'));
