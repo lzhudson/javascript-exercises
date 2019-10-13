70171347b97f8b8a61c42df9447830a3d23f9086
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-12 22:40:09 -0300

    Adicionando mais testes.

--- Javascript Basic/Question-22/app.js
+++ Javascript Basic/Question-22/app.js
@@ -2,3 +2,5 @@ function removeChar(string, valueToRemove) {
   return string.replace(string.charAt(valueToRemove), '');
 }
 console.log(removeChar('Hudson', 2));
+console.log(removeChar('Python', 3));
+console.log(removeChar('Python', 5));
