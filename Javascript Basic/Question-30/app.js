de97dfab70d2b604642813e42eb72c7ca157b939
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-14 10:30:50 -0300

    Alterando questão e adicionando mais uma linha de teste.

--- Javascript Basic/Question-30/app.js
+++ Javascript Basic/Question-30/app.js
@@ -1,10 +1,12 @@
 function isContentScript(string) {
-  if(string.includes('Script')) {
-    return string.replace('Script','');
+  if(string.length < 6) {
+    return string;
+  } else if(string.substring(10, 4) === 'Script') {
+    return string.substring(0, 4) + string.substring(10, string.length);
   } else {
     return string;
   }
-  
 }
 console.log(isContentScript('JavaScript'));
 console.log(isContentScript('JavaScriptES6'));
+console.log(isContentScript('CoffeScript'));
