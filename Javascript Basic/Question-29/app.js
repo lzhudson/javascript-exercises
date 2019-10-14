fb13ce7aa23b6b1579ae90b0fde33db03fd1fb3e
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-14 10:25:28 -0300

    Adicionando o terceiro numero que se pede na questão.

--- Javascript Basic/Question-29/app.js
+++ Javascript Basic/Question-29/app.js
@@ -1,5 +1,5 @@
-function inBetween50a99(value1, value2) {
-  if((value1 > 50 && value2 < 99) || (value2 > 50 && value2 < 99)) {
+function inBetween50a99(value1, value2, value3) {
+  if((value1 > 50 && value2 < 99) || (value2 > 50 && value2 < 99) || (value3 > 50 && value3 < 99)) {
       return true;
   } else {
     return false;
