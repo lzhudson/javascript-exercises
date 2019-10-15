07cdfebe63a7462a4ed05980580bafacc30dbc44
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-15 09:46:19 -0300

    Corrigindo retorno da função

--- Javascript Basic/Question-35/app.js
+++ Javascript Basic/Question-35/app.js
@@ -3,9 +3,9 @@ function isSpecifiedChar(string, char) {
   string.split('').forEach(element => {
     if(element === char) {
       ocurrence++;
-      console.log(ocurrence);
     }
   });
-  return ocurrence >= 3 ? `Is ocurrence equal ${ocurrence}` : 'Not ocurrence greater three.';
+  return (ocurrence >= 2 && ocurrence <=4);
 }
 console.log(isSpecifiedChar('oioioioi', 'i'));
+console.log(isSpecifiedChar('Coffescript', 'i'));
